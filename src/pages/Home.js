73da23b3a32bd34010b8608570dc1f3cb135e194
The Glass Corner <ksimons@aggies.ncat.edu>
2021-02-23 00:40:50 -0500

    Updated Home with accurate infomation, work xp etc.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -25,7 +25,7 @@ class Home extends React.Component {
         <p>Full Stack Web Developer<br />
         Kellik Simons </p>
       </header>
-      <span className="image"><img src="./assets/images/IMG_1551.jpg" alt="" /></span>
+      <span className="image"><img src="./assets/images/kellik.jpg" alt="" /></span>
     </div>
     <a href="#one" className="goto-next scrolly">Next</a>
   </section>
@@ -38,23 +38,20 @@ class Home extends React.Component {
           <div className="col-4 col-12-medium">
             <header>
               <h2>Background</h2>
-              <p>Nascetur eu nibh vestibulum amet gravida nascetur praesent</p>
+              <p>Hi I'm Kellik Simons, I am a Full Stack Web Developer & Computer Science Graduate from North Carolina A&T State University. </p>
             </header>
           </div>
           <div className="col-4 col-12-medium">
-            <p>Feugiat accumsan lorem eu ac lorem amet sed accumsan donec.
-            Blandit orci porttitor semper. Arcu phasellus tortor enim mi
-            nisi praesent dolor adipiscing. Integer mi sed nascetur cep aliquet
-            augue varius tempus lobortis porttitor accumsan consequat
-            adipiscing lorem dolor.</p>
+            <p>My professionalm background is in Software Development and Project Management. I also enjoy Gaming, Traveling and Drone Videography.
+                Web development has been my focus & passion and I enjoy learning & building using the latest technologies. </p>
           </div>
-          <div className="col-4 col-12-medium">
+          {/* <div className="col-4 col-12-medium">
             <p>Morbi enim nascetur et placerat lorem sed iaculis neque ante
             adipiscing adipiscing metus massa. Blandit orci porttitor semper.
             Arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer
             mi sed nascetur cep aliquet augue varius tempus. Feugiat lorem
             ipsum dolor nullam.</p>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
@@ -66,10 +63,14 @@ class Home extends React.Component {
     <span className="image fit main"><img src="./assets/images/pic03.jpg" alt="" /></span>
     <div className="content">
       <header>
-        <h2>Work Experience</h2>
-        <p>Nunc commodo accumsan eget id nisi eu col volutpat magna</p>
+        <h2>Professional Work Experience</h2>
+        <p>Bank of America GT&O Development Program Analyst</p>
       </header>
-      <p>Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor lorem et accumsan consequat adipiscing lorem.</p>
+      <p>During my time in the Global Technology & Operations Rotational Program at Bank of America, I have 
+          participated & contributed in a variety of roles. Those being heavily focused around Project Management, while 
+          using my computer science background to continue my interests in Web Development. I have been able to contribute to highly technical teams
+          using my technical expertise to effectively communicate and deliver highly active projects during my time. 
+      </p>
       <ul className="actions">
         <li><a href="#" className="button">Learn More</a></li>
       </ul>
@@ -82,9 +83,9 @@ class Home extends React.Component {
     <div className="content">
       <header>
         <h2>Education & Courses </h2>
-        <p>Accumsan integer ultricies aliquam vel massa sapien phasellus</p>
+        <p>North Carolina A&T State University <br/> Degree of Bachelors in Computer Science </p>
       </header>
-      <p>Feugiat accumsan lorem eu ac lorem amet ac arcu phasellus tortor enim mi mi nisi praesent adipiscing. Integer mi sed nascetur cep aliquet augue varius tempus lobortis porttitor lorem et accumsan consequat adipiscing lorem.</p>
+      <p>The Complete Web Developer in 2021: Zero To Mastery Academy - 2021 Udemy Course</p>
       <ul className="actions">
         <li><a href="#" className="button">Learn More</a></li>
       </ul>
@@ -96,38 +97,38 @@ class Home extends React.Component {
     <div className="container">
       <header className="major">
         <h2>Projects</h2>
-        <p>Iaculis ac volutpat vis non enim gravida nisi faucibus posuere arcu consequat</p>
+        <p>Below are Projects that I have enjoyed contributing to and/or am currently working on. </p>
       </header>
       <div className="box alt">
         <div className="row gtr-uniform">
           <section className="col-4 col-6-medium col-12-xsmall">
             <span className="icon solid alt major fa-chart-area"></span>
-            <h3>Ipsum sed commodo</h3>
+            <h3>Pet App</h3>
             <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
           </section>
           <section className="col-4 col-6-medium col-12-xsmall">
             <span className="icon solid alt major fa-comment"></span>
-            <h3>Eleifend lorem ornare</h3>
+            <h3>Goals React Native App</h3>
             <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
           </section>
           <section className="col-4 col-6-medium col-12-xsmall">
             <span className="icon solid alt major fa-flask"></span>
-            <h3>Cubilia cep lobortis</h3>
+            <h3>Great Day Ahead React Website</h3>
             <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
           </section>
           <section className="col-4 col-6-medium col-12-xsmall">
             <span className="icon solid alt major fa-paper-plane"></span>
-            <h3>Non semper interdum</h3>
+            <h3>React Portfolio Site</h3>
             <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
           </section>
           <section className="col-4 col-6-medium col-12-xsmall">
             <span className="icon solid alt major fa-file"></span>
-            <h3>Odio laoreet accumsan</h3>
+            <h3>Smart Brain Web Application</h3>
             <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
           </section>
           <section className="col-4 col-6-medium col-12-xsmall">
             <span className="icon solid alt major fa-lock"></span>
-            <h3>Massa arcu accumsan</h3>
+            <h3>Budget React Native App</h3>
             <p>Feugiat accumsan lorem eu ac lorem amet accumsan donec. Blandit orci porttitor.</p>
           </section>
         </div>
